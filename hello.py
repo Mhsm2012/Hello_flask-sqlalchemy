60c1b76d39200d4f0323c57e734fbb4012877d9f
mohamed <eliwa554@gmail.com>
2022-12-13 21:05:41 +0300

    run app

--- hello.py
+++ hello.py
@@ -7,4 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'hello world '
+    return 'hello world'
+
+if __name__ == '__main__':
+   
+   app.run(host="127.0.0.1", port = 1023)

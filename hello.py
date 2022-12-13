8325ca1512cabc7d65244c8d462d7c08c73a954f
mohamed <eliwa554@gmail.com>
2022-12-13 22:37:03 +0300

    create table in postgrel

--- hello.py
+++ hello.py
@@ -3,8 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 # import flask library
 # define flask as application through using flask (__name__)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgress@localhost:5432/example'
+# configure link to database 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/example'
 db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ =  'persons'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String ,nullable = False)
+
+db.create_all()
+
 
 
 
